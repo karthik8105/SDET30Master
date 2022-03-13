bdd4364b4396ca453cc4a85af50806c62ffef637
sreej <sreej@LAPTOP-5PVFR6I4>
2022-03-13 12:55:05 +0530

    Manager : first Commit

---
+++ src/test/java/Demomodule/DemoTestcasteTest.java
@@ -0,0 +1,12 @@
+package Demomodule;
+
+import org.testng.annotations.Test;
+
+public class DemoTestcasteTest
+{
+@Test
+public void demoTestcasteTest()
+{
+	System.out.println("Manager : first Commit");
+}
+}
